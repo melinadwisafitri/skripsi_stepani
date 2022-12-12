1c7c09ed2a02c2c2f0bb100000cb52ec4e2ce9a2
Melina Dwi Safitri <melina18dwi@gmail.com>
2022-12-12 13:07:57 +0700

    updata atribut/element

--- static/script/index.js
+++ static/script/index.js
@@ -1,3 +1,32 @@
+const humbergerswitch = document.querySelector('.menu-switch')
+const navbar = document.querySelector('.ul-list')
+
+humbergerswitch.addEventListener("click", () => {
+    humbergerswitch.classList.toggle('active');
+    navbar.classList.toggle('active');
+})
+
+
+function updatebg(){
+    const dhour = (new Date()).getHours()
+    const body = document.getElementById('home')
+    const bbody = body.style
+
+    if (dhour < 10){
+        bbody.background = "url('/static/pagi-01.svg')"
+        bbody.backgroundSize = 'cover'
+    } else if(dhour < 15){
+        bbody.background = "url('/static/siang-01.svg')"
+        bbody.backgroundSize = 'cover'
+    } else{
+        bbody.background = "url('/static/bg.svg')"
+        bbody.backgroundSize = 'cover'
+    }
+}
+
+setInterval(updatebg, 1000 * 60)
+updatebg();
+
 const button_predict = document.querySelector("#btn_submit");
 const btn_close = document.getElementById('btn-close')
 const input_bahan = document.getElementById('jenis_data_pangan')
@@ -5,21 +34,30 @@ const input_date = document.getElementById('date_end')
 
     
 button_predict.addEventListener("click", () => {
-    const data = document.querySelector('#jenis_data_pangan');
-    const date = document.querySelector(".date_end");
     const xhr = new XMLHttpRequest();
-    console.log(date)
-    console.log(Date.now())
+    
+    const datenow = new Date()
 
-    const timelapse = Date.now()
-    const today = new Date(timelapse)
-    console.log(today)
+    const formating = (date)=> {
+        let dates = new Date(date),
+        month = '' + (dates.getMonth() + 1),
+        day = '' + dates.getDate(),
+        year = dates.getFullYear();
 
-    if (input_bahan.value== '' || input_date.value == ''){
-        alert('fill bahan')
+        return [year, month, day].join('-')
     }
-    else if(input_date.value == Date.now() || input_date.value == Date.now()){
-        alert('isi tanggal setelah hari ini')
+
+    console.log(formating(datenow))
+
+    if(input_date.value < formating(datenow)){
+        console.log(input_date.value, formating(datenow))
+        alert('isi tanggal setelah tanggal hari ini')
+    }
+    else if(input_date.value == formating(datenow)){
+        alert('isi tanggal setelah tanggal hari ini')
+    }
+    else if (input_bahan.value == '' || input_date.value == ''){
+        alert('isi jenis bahan pangan dan tanggal waktu ingin prediksi harga')
     }
     else{
         xhr.onloadstart= () => {
@@ -46,7 +84,7 @@ button_predict.addEventListener("click", () => {
     
         xhr.open('POST', '/predict', true)
         xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded;charset=UTF-8')
-        xhr.send('bahan='+data.value+'&date='+date.value)
+        xhr.send('bahan='+input_bahan.value+'&date='+input_date.value)
     }
 })
 
@@ -70,22 +108,25 @@ const rendervalue =(msg) => {
     const card = document.querySelector('.value')
     card.innerHTML = `
     <div class="cards">
-        <div class="card-popup">
-            <button id="btn-close">&times;</button>
+      <div class="card-popup">
+        <div class="up">
             <h2>Hasil Prediksi</h2>
-            <img src="/plot" alt="plot predict">
-            <div id='chart'></div>
-            
-            <div class="line"></div>
-            <p>Prediksi harga ${msg.jenis_bahan}</p>
-            <p id="value">
-                pada tanggal ${msg.date} adalah RP${msg.predict}
-            </p>
+            <button id="btn-close">&times;</button>
         </div>
-    </div>
+          <img src="/plot" alt="plot predict">
+          <div id='chart'></div>
+          
+          <div class="line"></div>
+          <p>Prediksi harga ${msg.jenis_bahan}</p>
+          <p id="value">
+              pada tanggal ${msg.date} adalah RP${msg.predict}
+          </p>
+      </div>
+  </div>
     `
     const btn_close = document.getElementById('btn-close')
     btn_close.addEventListener('click', () => {
-        document.querySelector('.cards').style.display = 'none';
+        // document.querySelector('.cards').style.display = 'none';
+        window.location.reload();
     })
 }
